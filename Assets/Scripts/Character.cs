b5754be48f50302ebd1f129daa9618f92a67396f
JacobMunterSGD <148461541+JacobMunterSGD@users.noreply.github.com>
2025-01-03 14:28:14 -0500

    Base chariot movement is done!

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
 
+    [SerializeField] WheelCollider frontRight;
+    [SerializeField] WheelCollider frontLeft;
+    [SerializeField] WheelCollider backRight;
+    [SerializeField] WheelCollider backLeft;
+
+    public float acceleration;
+    public float breakingForce;
+
+    float currentAccel;
+    float currentBreakForce;
+
+
     CharacterController characterController;
 
     public float speed;
@@ -20,11 +32,42 @@ public class Character : MonoBehaviour
 
     void FixedUpdate()
     {
+        // input for forwards
+        if (Input.GetKey(KeyCode.W))
+        {
+            currentAccel = acceleration;
+        }
+        else
+        {
+            currentAccel = 0;
+        }
+
+        // braking
+        if (Input.GetKey(KeyCode.S))
+        {
+            currentBreakForce = breakingForce;
+        }
+        else
+        {
+            currentBreakForce = 0;
+        }
+
+        frontRight.motorTorque = currentAccel;
+        frontLeft.motorTorque = currentAccel;
+
+        frontRight.brakeTorque = currentBreakForce;
+        frontLeft.brakeTorque = currentBreakForce;
+        backRight.brakeTorque = currentBreakForce;
+        backLeft.brakeTorque = currentBreakForce;
+
         //Vector3 PlayerInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
         float ForwardDirection = Input.GetAxisRaw("Vertical");
 
         currentTurnAngle = maxTurnAngle * Input.GetAxis("Horizontal");
+
+        frontRight.steerAngle = currentTurnAngle;
+        frontLeft.steerAngle = currentTurnAngle;
 
         //characterController.Move(move * Time.deltaTime * speed);
 
