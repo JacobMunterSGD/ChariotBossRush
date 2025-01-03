ce39e01f58f0373779a0ee1816ef03316368a619
JacobMunterSGD <148461541+JacobMunterSGD@users.noreply.github.com>
2025-01-03 12:57:41 -0500

    Started on movement

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -9,16 +9,25 @@ public class Character : MonoBehaviour
 
     public float speed;
 
+    public float maxTurnAngle;
+
+    float currentTurnAngle;
+
     void Start()
     {
         characterController = GetComponent<CharacterController>();   
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        //Vector3 PlayerInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
-        characterController.Move(move * Time.deltaTime * speed);
+        float ForwardDirection = Input.GetAxisRaw("Vertical");
+
+        currentTurnAngle = maxTurnAngle * Input.GetAxis("Horizontal");
+
+        //characterController.Move(move * Time.deltaTime * speed);
 
     }
+
 }
