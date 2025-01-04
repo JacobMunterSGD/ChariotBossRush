1b2f42088d9716259ecee6fe8adef243b54a17b9
JacobMunterSGD <148461541+JacobMunterSGD@users.noreply.github.com>
2025-01-03 20:38:49 -0500

    added a basic slingshot attack

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -19,7 +19,23 @@ public class Character : MonoBehaviour
     public float maxTurnAngle;
     float currentTurnAngle;
 
+    [SerializeField] GameObject projectile;
+    [SerializeField] float projectileVerticalForce;
+    [SerializeField] float projectileHorizontalForce;
+
+
+    private void Update()
+    {
+        SlingshotAttack();
+
+    }
     void FixedUpdate()
+    {
+        ChariotAccelerateAndBraking();
+        ChariotTurning();
+    }
+
+    void ChariotAccelerateAndBraking()
     {
         // input for forwards
         if (Input.GetKey(KeyCode.W))
@@ -41,21 +57,43 @@ public class Character : MonoBehaviour
             currentBreakForce = 0;
         }
 
+        // accelerate
         frontRight.motorTorque = currentAccel;
         frontLeft.motorTorque = currentAccel;
 
+        // break
         frontRight.brakeTorque = currentBreakForce;
         frontLeft.brakeTorque = currentBreakForce;
         backRight.brakeTorque = currentBreakForce;
         backLeft.brakeTorque = currentBreakForce;
+    }
 
-        float ForwardDirection = Input.GetAxisRaw("Vertical");
-
+    void ChariotTurning()
+    {
+        // turning input
         currentTurnAngle = maxTurnAngle * Input.GetAxis("Horizontal");
 
+        // Turn wheels based on input
         frontRight.steerAngle = currentTurnAngle;
         frontLeft.steerAngle = currentTurnAngle;
+    }
 
+    void SlingshotAttack()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // instantiate "projectile"
+            GameObject _projectile = Instantiate(projectile, transform.position, Quaternion.identity);
+
+            // chariot current velocity
+            Vector3 currentChariotVelocity = gameObject.GetComponent<Rigidbody>().velocity;
+
+            // launch projectile
+            _projectile.GetComponent<Rigidbody>().AddForce(transform.forward * projectileHorizontalForce + new Vector3(0, projectileVerticalForce, 0) + currentChariotVelocity, ForceMode.Impulse);
+
+            // destroy after time
+            Destroy(_projectile, 5);
+        }
     }
 
 }
